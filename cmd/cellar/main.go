7abc61017b50c0f79bda0cfedbee47abc8b9404f
Parker Johansen <johansen.parker@gmail.com>
2020-12-05 02:14:44 -0700

    Update swagger info

--- cmd/cellar/main.go
+++ cmd/cellar/main.go
@@ -9,8 +9,14 @@ import (
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title Cellar
+// @description Simple secret sharing with the infrastructure you already trust
+
 // @contact.name Parker Johansen
 // @contact.email johansen.parker@gmail.com
+
+// @license.name MIT
+// @license.url https://gitlab.com/cellar-app/cellar-api/-/blob/148abea87dfbba32ab1aefc1ab36b2de1f652c9e/LICENSE.txt
 func main() {
 	router := gin.New()
 	cfg := settings.NewConfiguration()
--- pkg/middleware/swagger.go
+++ pkg/middleware/swagger.go
@@ -7,8 +7,6 @@ import (
 )
 
 func configureSwagger(configuration settings.IConfiguration) {
-	docs.SwaggerInfo.Title = "Cellar"
-	docs.SwaggerInfo.Description = "Secure secret sharing for families"
 	docs.SwaggerInfo.Host = strings.TrimLeft(configuration.App().ClientAddress(), "http://")
 	docs.SwaggerInfo.BasePath = "/"
 }
